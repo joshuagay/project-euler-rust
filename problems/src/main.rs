7e6ee0d7dc4aadabc86236dd7fd76daf87ce35b9
Joshua Gay <joshuagay@gmail.com>
2017-03-31 16:46:08 -0600

    looping

--- problems/src/main.rs
+++ problems/src/main.rs
@@ -2,16 +2,20 @@ use std::io;
 
 fn main() {
     println!("This is Josh's Project Euler in Rust program");
-    println!("Enter the number of the problem you wold like to run:");
 
-    let mut eulerproblem = String::new();
+    loop {		   
+        println!("Enter the number of the problem you wold like to run:");
 
-    io::stdin().read_line(&mut eulerproblem)
-        .expect("Failed to read line");
+        let mut eulerproblem = String::new();
 
-    let guess: u32 = eulerproblem.trim().parse()
-        .expect("Please type a number!");
+        io::stdin().read_line(&mut eulerproblem)
+            .expect("Failed to read line");
 
+        let eulerproblem: u32 = match eulerproblem.trim().parse() {
+            Ok(num) => num,
+	    Err(_) => continue,
+        };
+    }
 
     println!("Running: {}", eulerproblem);
     println!("...");	
